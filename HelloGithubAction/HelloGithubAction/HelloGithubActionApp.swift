45e2183d90db1dfe57ca93b396eadd1e21823a56
tienvv <iostienvv@gmail.com>
2023-07-12 12:00:58 +0700

    setup project

---
+++ HelloGithubAction/HelloGithubAction/HelloGithubActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGithubActionApp.swift
+//  HelloGithubAction
+//
+//  Created by Tienvv on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloGithubActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
